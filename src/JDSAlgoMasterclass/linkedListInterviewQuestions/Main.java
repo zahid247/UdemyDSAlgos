d37124f25860cc072cbc32f1eb1d7fc2a01d1320
Zahid Aliyev <zahid.aliyev@outlook.com>
2024-06-07 11:25:56 -0700

    Delete duplicates method has been completed successfully

--- src/JDSAlgoMasterclass/linkedListInterviewQuestions/Main.java
+++ src/JDSAlgoMasterclass/linkedListInterviewQuestions/Main.java
@@ -8,9 +8,18 @@ public class Main {
         ll.createLL(1);
         ll.insertNode(2);
         ll.insertNode(3);
+        ll.insertNode(3);
         ll.insertNode(4);
         ll.insertNode(5);
+        ll.insertNode(1);
+        ll.insertNode(1);
+        ll.insertNode(2);
         ll.traversalLL();
+
+        Questions q = new Questions();
+        System.out.println(q.deleteDups(ll));
+        ll.traversalLL();
+
     }
 
 }
