fc9b2f65cdf3b4e1ea60eebd7904a196b0daed9c
Zahid Aliyev <zahaliye@amazon.com>
2024-08-26 13:50:12 -0700

    added a new leetcode exercise for finding an avgs of the given array. but the solution is not yet configured properly

---
+++ src/JDSAlgoMasterclass/leetcode/ReverseWordsInString.java
@@ -0,0 +1,24 @@
+package JDSAlgoMasterclass.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseWordsInString {
+
+    public static void main(String[] args) {
+        ReverseWordsInString rw = new ReverseWordsInString();
+
+
+        String s = "the sky is blue";
+
+    }
+
+    public String reverseWords(String s) {
+
+
+
+        return "";
+
+    }
+
+}
