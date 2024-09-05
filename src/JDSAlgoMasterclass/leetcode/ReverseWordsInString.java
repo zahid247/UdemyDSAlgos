050c711e7a3146c3d502396537dcd7866984003d
Zahid Aliyev <zahaliye@amazon.com>
2024-09-05 00:16:03 -0700

    Added few more new methods. There is one though that I did not quite understand how to solve. Will need to come back to that. subarraySum function seems little too complicated

--- src/JDSAlgoMasterclass/leetcode/ReverseWordsInString.java
+++ src/JDSAlgoMasterclass/leetcode/ReverseWordsInString.java
@@ -20,9 +20,106 @@ public class ReverseWordsInString {
 //        System.out.println(Arrays.toString(rw.moveZeroes(new int[]{0, 1, 0, 3, 12})));
 //        System.out.println(rw.reversePrefix("abcdefg", 'd'));
 
-        System.out.println(rw.minSubArrayLen(7, new int[] {2,3,1,2,4,3}));
+//        System.out.println(rw.minSubArrayLen(7, new int[] {2,3,1,2,4,3}));
+//        System.out.println(rw.maxVowels("abciiidef", 3));
+//        System.out.println(rw.intersection(new int[][]{ {3,1,2,4,5} , {1,2,3,4} , {3,4,5,6} } ));
+
+//        System.out.println(rw.subarraySum(new int[] {1,2,1,2,1}, 3));
+
+
+        System.out.println(2174 & 1); /* the & symbol is the bitwise AND operator.
+        It performs an AND operation on the binary representation of the two operands.
+        In this case, num & 1 performs a bitwise AND between the integer num and 1. The bitwise
+        AND operation compares each bit of the two numbers and returns a 1 only when both
+        corresponding bits are 1; otherwise, it returns 0.*/
+    }
+
+
+
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> counts = new HashMap<>();
+        counts.put(0, 1);
+
+        int ans=0, curr=0;
+
+        for (int num: nums) {
+            System.out.println("num = " + num);
+
+            curr += num;
+            System.out.println("curr = " + curr);
+
+            ans += counts.getOrDefault(curr - k, 0);
+            System.out.println("counts.getOrDefault(curr - k, 0) = " + counts.getOrDefault(curr - k, 0));
+            System.out.println("ans = " + ans);
+
+            counts.put(curr, counts.getOrDefault(curr, 0) + 1);
+            System.out.println("counts = " + counts);
+
+            System.out.println("-----NEXT ITERATION STARTS-----");
+        }
+        System.out.println("return ans = " + ans);
+        return ans;
+    }
+
+
+
+    public boolean areOccurrencesEqual(String s) {
+
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        Set<Integer> frequencies = new HashSet<>(counts.values());
+        return frequencies.size() == 1;
+    }
+
+    public List<Integer> intersection(int[][] nums) {
+
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for(int[] arr: nums)
+            for(int j: arr)
+                counts.put(j, counts.getOrDefault(j,0)+1);
+
+        int len = nums.length;
+        List<Integer> ans = new ArrayList<>();
+
+        for(int key: counts.keySet()){
+            if(counts.get(key) == len) ans.add(key);
+        }
+
+        Collections.sort(ans);
+        return ans;
 
     }
+
+    public int maxVowels(String s, int k) {
+
+        int length = s.length();
+        int numVowels = 0;
+        int left = 0;
+        int right = 0;
+        int res = 0;
+
+        while (left < length - k+1){
+            while (right < left + k){
+                char c = s.charAt(right);
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    numVowels++;
+                }
+                right++;
+            }
+            res = Math.max(res, numVowels);
+            left++;
+            right = left;
+            numVowels = 0;
+        }
+        return res;
+    }
+
+
 
     //This is not completed yet, still failing some test cases
     public int minSubArrayLen(int target, int[] nums) {
